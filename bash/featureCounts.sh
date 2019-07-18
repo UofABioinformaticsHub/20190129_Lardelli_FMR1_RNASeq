302f1fe3635a383de2c3aba8d527a3d1c04c261a
steveped <stephen.pederson.au@gmail.com>
2019-07-18 19:39:41 +0930

    Added all Melanie's results files from s2 exonic, but renamed as the only run

--- bash/featureCounts.sh
+++ bash/featureCounts.sh
@@ -21,8 +21,8 @@ PROJROOT=/fast/users/a1222913/20190129_Lardelli_FMR1_RNASeq
 ALIGNDATA=${PROJROOT}/2_alignedData
 
 ## Genomic Data Files
-REFS=/data/biorefs/reference_genomes/ensembl-release-94/danio-rerio/
-GTF=${REFS}/Danio_rerio.GRCz11.94.chr.gtf
+REFS=/data/biorefs/reference_genomes/ensembl-release-94/danio-rerio
+GTF=${REFS}/Danio_rerio.GRCz11.94.chr.gtf.gz
 
 ## Feature Counts - obtaining all sorted bam files
 sampleList=`find ${ALIGNDATA}/bams -name "*out.bam" | tr '\n' ' '` #creates list of bam files, replacing new line with a space
@@ -30,12 +30,14 @@ sampleList=`find ${ALIGNDATA}/bams -name "*out.bam" | tr '\n' ' '` #creates list
 ## Running featureCounts on the sorted bam files 
 #-Q min quality read score, 
 #-s strand specific 0=unstranded, 1= stranded, 2 = reversely stranded
+#--fracOverlap is minimum fraction of overlapping bases in a read required for read assignment (1 = 100%)
+#-t feature type in GTF to use for read counting
 #-a annotation file
 #-o output file name
-featureCounts -Q 10 -s 0 -T ${CORES} -a ${GTF} -o ${ALIGNDATA}/featureCounts/counts.out ${sampleList}
+featureCounts -Q 10 -s 2 --fracOverlap 1 -t exon -T ${CORES} -a ${ALIGNDATA}/Danio_rerio.GRCz11.94.chr.gtf -F GTF -o ${ALIGNDATA}/featureCounts/counts.s2.exonic.out ${sampleList}
 
 
 ## Remove columns 2-6 and row 1 of the output
-cut -f1,7- ${ALIGNDATA}/featureCounts/counts.out | \
-sed 1d > ${ALIGNDATA}/featureCounts/genes.out
+cut -f1,7- ${ALIGNDATA}/featureCounts/counts.s2.exonic.out | \
+sed 1d > ${ALIGNDATA}/featureCounts/genes.s2.exonic.out
 
